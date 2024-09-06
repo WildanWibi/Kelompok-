19e5143bef96c26f50dc17eed4871e5c70b5045d
daspro-obsheet2 <wildanwibisono52@gmail.com>
2024-09-06 17:40:51 +0700

    Kelompok GitHub

---
+++ KelompokGitHub.java
@@ -0,0 +1,8 @@
+public class KelompokGitHub {
+    public static void main(String[] args) {
+        System.out.println("Nama    : M Wildan Wibisono");
+        System.out.println("Nim     : 244107060118");
+        System.out.println("Kelas   : SIB 1C");
+        System.out.println("Hobby   : Bermain Game");
+    }
+}
