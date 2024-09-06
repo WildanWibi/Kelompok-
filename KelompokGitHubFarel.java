92026e6bcd67abda5cf0f1a16e1b5804cf5a586f
daspro-obsheet2 <wildanwibisono52@gmail.com>
2024-09-07 06:34:04 +0700

    change

---
+++ KelompokGitHubFarel.java
@@ -0,0 +1,8 @@
+public class KelompokGitHubFarel {
+    public static void main(String[] args) {
+        System.out.println("Nama    : Almafarel Akbar Remizard");
+        System.out.println("Nim     : 244107060019");
+        System.out.println("Kelas   : SIB 1C");
+        System.out.println("Hobby   : Bersepeda");
+    }
+}
---
+++ KelompokGitHubSulton.java
@@ -0,0 +1,8 @@
+public class KelompokGitHubSulton {
+    public static void main(String[] args) {
+    System.out.println("Nama    :");
+    System.out.println("Nim     :");
+    System.out.println("Kelas   :");
+    System.out.println("Hobby   :");
+    }
+}
