7b6838c2089078410e9f4a79c5426508fa826df3
daspro-obsheet2 <wildanwibisono52@gmail.com>
2024-09-07 07:22:50 +0700

    Kelompok GitHub

--- KelompokGitHubSulton.java
+++ KelompokGitHubSulton.java
@@ -1,8 +1,8 @@
 public class KelompokGitHubSulton {
     public static void main(String[] args) {
-         System.out.println("Nama    : Sultan Syarif Ubaidillah Al Nashr");
-         System.out.println("Nim     : 244107060151");
-         System.out.println("Kelas   : SIB 1C");
-         System.out.println("Hobby   : Rebahan");
+        System.out.println("Nama    : Sultan Syarif Ubaidillah Al Nashr");
+        System.out.println("Nim     : 244107060151");
+        System.out.println("Kelas   : SIB 1C");
+        System.out.println("Hobby   : Rebahan");
     }
 }
